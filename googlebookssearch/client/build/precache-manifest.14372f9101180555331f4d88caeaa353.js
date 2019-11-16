75dc86b4d01ccf520d36a240b8b479732e3f0f58
LMBoyle <boylelukem@gmail.com>
2019-11-15 21:15:41 -0500

    ran build

---
+++ googlebookssearch/client/build/precache-manifest.14372f9101180555331f4d88caeaa353.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cd9b6ec2fbb0ea33c587",
+    "url": "/static/js/main.23c8cb6e.chunk.js"
+  },
+  {
+    "revision": "f2179e7f22e6577b35cf",
+    "url": "/static/js/2.565b7cae.chunk.js"
+  },
+  {
+    "revision": "cd9b6ec2fbb0ea33c587",
+    "url": "/static/css/main.4a352c47.chunk.css"
+  },
+  {
+    "revision": "5fce20d77c798ac9e68bc2c17295a2e9",
+    "url": "/index.html"
+  }
+];
