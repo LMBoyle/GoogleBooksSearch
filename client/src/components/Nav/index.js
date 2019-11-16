141c4f54cd17daf5785a636490b2b7755719bb17
LMBoyle <boylelukem@gmail.com>
2019-11-16 11:03:03 -0500

    updated styles on navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,15 +1,21 @@
 // Imports ========================================================================================
 
 import React from "react";
+import { Container } from "../Grid";
+
 
 // Functions ======================================================================================
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        Google Books Search
-      </a>
+    <nav className="navbar navbar-dark bg-dark">
+      <Container>
+        <a href="#" className="navbar-brand navbar-dark"> Google Books Search </a>
+        <form className="form-inline">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search For A Book" aria-label="Search" />
+          <button className="btn btn-outline-info my-2 my-sm-0" type="submit"> Search </button>
+        </form>
+      </Container>
     </nav>
   );
 }
